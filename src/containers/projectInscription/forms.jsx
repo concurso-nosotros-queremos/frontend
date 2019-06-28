f8408adcf6f00093ae1991b06f9c67483181c7e2
Francesco Silvetti <francescosilvetti8@gmail.com>
2019-06-28 08:32:24 -0300

    Add raw_participants table

--- src/containers/projectInscription/forms.jsx
+++ src/containers/projectInscription/forms.jsx
@@ -6,26 +6,42 @@ export default class InscriptionWrapper extends Component {
     super(props)
 
     this.state = {
-      fragments: [
-        (<ParticipantsWrapper />),
-        (<div>2</div>),
-        (<div>3</div>)
-      ],
       index: 0,
-      form: {}
+      form: {
+        raw_participants: []
+      }
     }
+
+    this.formHandler = this.formHandler.bind(this)
+  }
+
+  formHandler (name, value) {
+    console.log(this)
+    this.setState((state) => {
+      return { form: {
+        ...state.form,
+        [name]: value
+      }}
+    })
   }
 
   render () {
+    let fragments = [
+      (<ParticipantsWrapper rawParticipants={this.state.form.raw_participants} formHandler={this.formHandler} />),
+      (<div>2</div>),
+      (<div>3</div>)
+    ]
+
     return (
       <div>
-        {this.state.fragments[this.state.index]}
+        {console.log(this.state)}
+        {fragments[this.state.index]}
         {this.state.index !== 0 ? <button onClick={() => {
           this.setState((state) => {
             return { index: state.index - 1 }
           })
         }}>Atras</button> : null}
-        {this.state.index !== this.state.fragments.length - 1 ? <button onClick={() => {
+        {this.state.index !== fragments.length - 1 ? <button onClick={() => {
           this.setState((state) => {
             return { index: state.index + 1 }
           })
--- src/containers/projectInscription/forms/participantsWrapper.jsx
+++ src/containers/projectInscription/forms/participantsWrapper.jsx
@@ -5,6 +5,7 @@ import Button from '@material-ui/core/Button'
 import TextField from '@material-ui/core/TextField'
 import MenuItem from '@material-ui/core/MenuItem'
 import Checkbox from '@material-ui/core/Checkbox'
+import { Paper, Table, TableRow, TableCell, TableHead } from '@material-ui/core'
 
 const participantSchema = Yup.object().shape({
   first_name: Yup.string().required(),
@@ -19,7 +20,7 @@ const participantSchema = Yup.object().shape({
 export class ParticipantsWrapper extends Component {
   constructor (props) {
     super(props)
-    this.state = { raw_participants: [] }
+    this.state = { rawParticipants: props.rawParticipants }
   }
 
   handleSubmit (values) {
@@ -28,7 +29,9 @@ export class ParticipantsWrapper extends Component {
     participantSchema.isValid(values).then((valid) => {
       if (valid) {
         this.setState((state) => {
-          return { raw_participants: state.raw_participants.concat([participantSchema.cast(values)]) }
+          let newState = state.rawParticipants.concat([participantSchema.cast(values)])
+          this.props.formHandler('raw_participants', newState)
+          return { rawParticipants: newState }
         })
       }
     })
@@ -38,37 +41,40 @@ export class ParticipantsWrapper extends Component {
     let name = event.target.name
     let value = event.target.value
     this.setState((state) => {
-      let rawParticipants = state.raw_participants
+      let rawParticipants = state.rawParticipants
       rawParticipants[index][name] = value
-      return { raw_participants: rawParticipants }
+      return { rawParticipants: rawParticipants }
     })
   }
 
   handleDelete (index) {
-    if (window.confirm(`Seguro que quiere borrar a ${this.state.raw_participants[index].first_name}?`)) {
+    if (window.confirm(`Seguro que quiere borrar a ${this.state.rawParticipants[index].first_name}?`)) {
       this.setState((state) => {
-        let rawParticipants = state.raw_participants
+        let rawParticipants = state.rawParticipants
         rawParticipants.splice(index, 1)
-        return { raw_participants: rawParticipants }
+        return { rawParticipants: rawParticipants }
       })
     }
   }
 
   handleCheck (index, isChecked) {
     this.setState((state) => {
-      let rawParticipants = state.raw_participants
+      let rawParticipants = state.rawParticipants
       rawParticipants[index].isChecked = isChecked
       if (!isChecked) {
         rawParticipants[index].phone_number = ''
         rawParticipants[index].email = ''
       }
-      return { raw_participants: rawParticipants }
+      return { rawParticipants: rawParticipants }
     })
   }
 
   render () {
     return (
-      <div>
+      <Paper>
+        {console.log(this.props.rawParticipants)}
+        <h1>Lista de participantes</h1>
+        <p>Por favor introduzca los campos obligatorios (*):</p>
         <Formik
           initialValues={{
             first_name: '',
@@ -82,17 +88,24 @@ export class ParticipantsWrapper extends Component {
           validationSchema={participantSchema}
           onSubmit={(values, { resetForm }) => {
             this.handleSubmit(values)
-            resetForm()
+            resetForm({
+              first_name: '',
+              last_name: '',
+              dni: '',
+              email: '',
+              phone_number: '',
+              grade_choices: values.grade_choices,
+              divition_choices: values.divition_choices
+            })
           }}
         >
           {({ errors, touched }) => (
             <Form>
-              {console.log(this.state.raw_participants)}
               <Field
                 type='text'
                 name='first_name'
                 render={({ field }) => (
-                  <TextField {...field} label='Nombre*' variant='outlined' fullWidth
+                  <TextField {...field} label='Nombre *' variant='outlined'
                     error={errors.first_name && touched.first_name}
                     helperText={errors.first_name && touched.first_name ? errors.first_name : null} />)}
               />
@@ -100,7 +113,7 @@ export class ParticipantsWrapper extends Component {
                 type='text'
                 name='last_name'
                 render={({ field }) => (
-                  <TextField {...field} label='Apellido*' variant='outlined' fullWidth
+                  <TextField {...field} label='Apellido *' variant='outlined'
                     error={errors.last_name && touched.last_name}
                     helperText={errors.last_name && touched.last_name ? errors.last_name : null} />)}
               />
@@ -108,28 +121,28 @@ export class ParticipantsWrapper extends Component {
                 type='text'
                 name='dni'
                 render={({ field }) => (
-                  <TextField {...field} label='D.N.I.' variant='outlined' fullWidth
+                  <TextField {...field} label='D.N.I.' variant='outlined'
                     error={errors.dni && touched.dni}
                     helperText={errors.dni && touched.dni ? errors.dni : null} />)}
               />
               <Field
                 name='grade_choices'
                 render={({ field }) => (
-                  <TextField {...field} select label='Año*' variant='outlined'
+                  <TextField {...field} select label='Año *' variant='outlined'
                     error={errors.grade_choices && touched.grade_choices}
                     helperText={errors.grade_choices && touched.grade_choices ? errors.grade_choices : null}
                   >
-                    <MenuItem value='3'>7</MenuItem>
-                    <MenuItem value='2'>6</MenuItem>
-                    <MenuItem value='1'>5</MenuItem>
-                    <MenuItem value='0'>4</MenuItem>
+                    <MenuItem value='3'>7mo</MenuItem>
+                    <MenuItem value='2'>6to</MenuItem>
+                    <MenuItem value='1'>5to</MenuItem>
+                    <MenuItem value='0'>4to</MenuItem>
                   </TextField>
                 )}
               />
               <Field
                 name='divition_choices'
                 render={({ field }) => (
-                  <TextField {...field} select label='Curso*' variant='outlined'
+                  <TextField {...field} select label='Curso *' variant='outlined'
                     error={errors.divition_choices && touched.divition_choices}
                     helperText={errors.divition_choices && touched.divition_choices ? errors.divition_choices : null}
                   >
@@ -139,24 +152,51 @@ export class ParticipantsWrapper extends Component {
                   </TextField>
                 )}
               />
-              <Button type='submit'>Submit</Button>
+              <Button type='submit'>Añadir</Button>
             </Form>
           )}
         </Formik>
-        <ol>
-          {this.state.raw_participants.map((participant, index) => (
-            <li key={index}>{participant.first_name}
-              <Button onClick={() => { this.handleDelete(index) }}>Borrar</Button>
-              <Checkbox value={this.state.raw_participants[index].isChecked} onChange={(e, isChecked) => { this.handleCheck(index, isChecked) }} />
-              {this.state.raw_participants[index].isChecked
-                ? <div>
-                  <TextField required name='phone_number' label='Telefono' variant='outlined' onChange={(e) => { this.handleUpdate(e, index) }} />
-                  <TextField required name='email' label='Email' variant='outlined' onChange={(e) => { this.handleUpdate(e, index) }} />
-                </div> : null}
-            </li>))
-          }
-        </ol>
-      </div>
+        {this.state.rawParticipants.length > 0
+          ? <Table size='small'>
+            <TableHead>
+              <TableCell>Nombre</TableCell>
+              <TableCell>Apellido</TableCell>
+              <TableCell>D.N.I.:</TableCell>
+              <TableCell>Año</TableCell>
+              <TableCell>Curso</TableCell>
+              <TableCell>¿Contacto?</TableCell>
+              <TableCell>Campos opcionales</TableCell>
+              <TableCell>Acciones</TableCell>
+            </TableHead>
+            {this.state.rawParticipants.slice(0).reverse().map((participant, index) => (
+              <TableRow key={index}>
+                <TableCell>{participant.first_name}</TableCell>
+                <TableCell>{participant.last_name}</TableCell>
+                <TableCell>{participant.dni}</TableCell>
+                <TableCell>{participant.grade_choices}</TableCell>
+                <TableCell>{participant.divition_choices}</TableCell>
+                <TableCell>
+                  <Checkbox value={this.state.rawParticipants[index].isChecked} onChange={(e, isChecked) => { this.handleCheck(index, isChecked) }} />
+                </TableCell>
+                <TableCell>
+                  {this.state.rawParticipants[index].isChecked
+                    ? <div>
+                      <TextField name='phone_number' label='Telefono' onChange={(e) => { this.handleUpdate(e, index) }} />
+                      <TextField name='email' label='Email' onChange={(e) => { this.handleUpdate(e, index) }} />
+                    </div>
+                    : <div>
+                      <TextField disabled label='Telefono' />
+                      <TextField disabled label='Email' />
+                    </div>
+                  }
+                </TableCell>
+                <TableCell>
+                  <Button onClick={() => { this.handleDelete(index) }}>Borrar</Button>
+                </TableCell>
+              </TableRow>))
+            }
+          </Table> : null}
+      </Paper>
     )
   }
 }
