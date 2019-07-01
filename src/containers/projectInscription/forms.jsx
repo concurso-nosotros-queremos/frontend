20acc76ed0073ea30a03d4cb1e552af0fcfb0447
Francesco Silvetti <francescosilvetti8@gmail.com>
2019-07-01 17:57:26 -0300

    Hot save before fixing all the mess

--- src/containers/projectInscription/forms.jsx
+++ src/containers/projectInscription/forms.jsx
@@ -2,50 +2,10 @@ import React, { Component } from 'react'
 import { ParticipantsWrapper } from './forms/participantsWrapper'
 
 export default class InscriptionWrapper extends Component {
-  constructor (props) {
-    super(props)
-
-    this.state = {
-      index: 0,
-      form: {
-        raw_participants: []
-      }
-    }
-
-    this.formHandler = this.formHandler.bind(this)
-  }
-
-  formHandler (name, value) {
-    console.log(this)
-    this.setState((state) => {
-      return { form: {
-        ...state.form,
-        [name]: value
-      }}
-    })
-  }
-
   render () {
-    let fragments = [
-      (<ParticipantsWrapper rawParticipants={this.state.form.raw_participants} formHandler={this.formHandler} />),
-      (<div>2</div>),
-      (<div>3</div>)
-    ]
-
     return (
       <div>
-        {console.log(this.state)}
-        {fragments[this.state.index]}
-        {this.state.index !== 0 ? <button onClick={() => {
-          this.setState((state) => {
-            return { index: state.index - 1 }
-          })
-        }}>Atras</button> : null}
-        {this.state.index !== fragments.length - 1 ? <button onClick={() => {
-          this.setState((state) => {
-            return { index: state.index + 1 }
-          })
-        }}>Siguiente</button> : null}
+        <ParticipantsWrapper />
       </div>
     )
   }
--- src/containers/projectInscription/forms/participantsWrapper.jsx
+++ src/containers/projectInscription/forms/participantsWrapper.jsx
@@ -1,20 +1,24 @@
 import React, { Component } from 'react'
-import { Formik, Form, Field } from 'formik'
+import { Formik, Form, Field, FieldArray } from 'formik'
 import * as Yup from 'yup'
+import { Paper, Table, TableRow, TableCell, TableHead, TableBody } from '@material-ui/core'
 import Button from '@material-ui/core/Button'
 import TextField from '@material-ui/core/TextField'
 import MenuItem from '@material-ui/core/MenuItem'
-import Checkbox from '@material-ui/core/Checkbox'
-import { Paper, Table, TableRow, TableCell, TableHead } from '@material-ui/core'
+import AddOutlined from '@material-ui/icons/AddOutlined'
 
 const participantSchema = Yup.object().shape({
-  first_name: Yup.string().required(),
-  last_name: Yup.string().required(),
-  dni: Yup.string(),
-  email: Yup.string().email(),
-  phone_number: Yup.string(),
-  grade_choices: Yup.number().integer().min(0).max(3).required(),
-  divition_choices: Yup.number().integer().min(0).max(2).required()
+  raw_participants: Yup.array().of(
+    Yup.object().shape({
+      first_name: Yup.string().required('Nombre es un campo obligatorio'),
+      last_name: Yup.string().required('Apeliido es un campo obligatorio'),
+      dni: Yup.string(),
+      email: Yup.string().email(),
+      phone_number: Yup.string(),
+      grade_choices: Yup.number('Seleccione un año correcto').typeError('Seleccione un curso correcto').required('Año es un campo obligatorio'),
+      divition_choices: Yup.number().typeError('Seleccione un curso correcto').required('Curso es un campo obligatorio')
+    })
+  ).required().min(1)
 })
 
 export class ParticipantsWrapper extends Component {
@@ -23,50 +27,9 @@ export class ParticipantsWrapper extends Component {
     this.state = { rawParticipants: props.rawParticipants }
   }
 
-  handleSubmit (values) {
-    values.isChecked = false
-
-    participantSchema.isValid(values).then((valid) => {
-      if (valid) {
-        this.setState((state) => {
-          let newState = state.rawParticipants.concat([participantSchema.cast(values)])
-          this.props.formHandler('raw_participants', newState)
-          return { rawParticipants: newState }
-        })
-      }
-    })
-  }
-
-  handleUpdate (event, index) {
-    let name = event.target.name
-    let value = event.target.value
-    this.setState((state) => {
-      let rawParticipants = state.rawParticipants
-      rawParticipants[index][name] = value
-      return { rawParticipants: rawParticipants }
-    })
-  }
-
-  handleDelete (index) {
-    if (window.confirm(`Seguro que quiere borrar a ${this.state.rawParticipants[index].first_name}?`)) {
-      this.setState((state) => {
-        let rawParticipants = state.rawParticipants
-        rawParticipants.splice(index, 1)
-        return { rawParticipants: rawParticipants }
-      })
-    }
-  }
-
-  handleCheck (index, isChecked) {
-    this.setState((state) => {
-      let rawParticipants = state.rawParticipants
-      rawParticipants[index].isChecked = isChecked
-      if (!isChecked) {
-        rawParticipants[index].phone_number = ''
-        rawParticipants[index].email = ''
-      }
-      return { rawParticipants: rawParticipants }
-    })
+  getNestedObject (pathArr, nestedObj) {
+    return pathArr.reduce((obj, key) =>
+      (obj && obj[key] !== 'undefined') ? obj[key] : undefined, nestedObj)
   }
 
   render () {
@@ -77,125 +40,120 @@ export class ParticipantsWrapper extends Component {
         <p>Por favor introduzca los campos obligatorios (*):</p>
         <Formik
           initialValues={{
-            first_name: '',
-            last_name: '',
-            dni: '',
-            email: '',
-            phone_number: '',
-            grade_choices: '',
-            divition_choices: ''
-          }}
-          validationSchema={participantSchema}
-          onSubmit={(values, { resetForm }) => {
-            this.handleSubmit(values)
-            resetForm({
+            raw_participants: [{
               first_name: '',
               last_name: '',
               dni: '',
-              email: '',
-              phone_number: '',
-              grade_choices: values.grade_choices,
-              divition_choices: values.divition_choices
-            })
+              grade_choices: '',
+              divition_choices: ''
+            }]
+          }}
+          validationSchema={participantSchema}
+          onSubmit={(values) => {
+            window.alert(JSON.stringify(values, 0, 2))
           }}
         >
           {({ errors, touched }) => (
             <Form>
-              <Field
-                type='text'
-                name='first_name'
-                render={({ field }) => (
-                  <TextField {...field} label='Nombre *' variant='outlined'
-                    error={errors.first_name && touched.first_name}
-                    helperText={errors.first_name && touched.first_name ? errors.first_name : null} />)}
-              />
-              <Field
-                type='text'
-                name='last_name'
-                render={({ field }) => (
-                  <TextField {...field} label='Apellido *' variant='outlined'
-                    error={errors.last_name && touched.last_name}
-                    helperText={errors.last_name && touched.last_name ? errors.last_name : null} />)}
-              />
-              <Field
-                type='text'
-                name='dni'
-                render={({ field }) => (
-                  <TextField {...field} label='D.N.I.' variant='outlined'
-                    error={errors.dni && touched.dni}
-                    helperText={errors.dni && touched.dni ? errors.dni : null} />)}
-              />
-              <Field
-                name='grade_choices'
-                render={({ field }) => (
-                  <TextField {...field} select label='Año *' variant='outlined'
-                    error={errors.grade_choices && touched.grade_choices}
-                    helperText={errors.grade_choices && touched.grade_choices ? errors.grade_choices : null}
-                  >
-                    <MenuItem value='3'>7mo</MenuItem>
-                    <MenuItem value='2'>6to</MenuItem>
-                    <MenuItem value='1'>5to</MenuItem>
-                    <MenuItem value='0'>4to</MenuItem>
-                  </TextField>
+              {console.log(errors)}
+              <FieldArray
+                name='raw_participants'
+                render={(arrayHelpers) => (
+                  <Table size='small'>
+                    <TableHead>
+                      <TableRow>
+                        <TableCell>Nombre</TableCell>
+                        <TableCell>Apellido</TableCell>
+                        <TableCell>D.N.I.</TableCell>
+                        <TableCell>Año</TableCell>
+                        <TableCell>Curso</TableCell>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      {arrayHelpers.form.values.raw_participants.slice(0).reverse().map((participant, index) => (
+                        <TableRow key={index}>
+                          <TableCell>
+                            <Field
+                              name={`raw_participants.${index}.first_name`}
+                              render={({ field }) => (
+                                <TextField {...field} label='Nombre *'
+                                  error={this.getNestedObject(['raw_participants', index, 'first_name'], errors) && this.getNestedObject(['raw_participants', index, 'first_name'], touched)}
+                                  helperText={this.getNestedObject(['raw_participants', index, 'first_name'], errors) && this.getNestedObject(['raw_participants', index, 'first_name'], touched) ? this.getNestedObject(['raw_participants', index, 'first_name'], errors) : null} />)}
+                            />
+                          </TableCell>
+                          <TableCell>
+                            <Field
+                              name={`raw_participants.${index}.last_name`}
+                              render={({ field }) => (
+                                <TextField {...field} label='Apellido *'
+                                  error={this.getNestedObject(['raw_participants', index, 'last_name'], errors) && this.getNestedObject(['raw_participants', index, 'last_name'], touched)}
+                                  helperText={this.getNestedObject(['raw_participants', index, 'last_name'], errors) && this.getNestedObject(['raw_participants', index, 'last_name'], touched) ? this.getNestedObject(['raw_participants', index, 'last_name'], errors) : null} />)}
+                            />
+                          </TableCell>
+                          <TableCell>
+                            <Field
+                              name={`raw_participants.${index}.dni`}
+                              render={({ field }) => (
+                                <TextField {...field} label='D.N.I.'
+                                  error={this.getNestedObject(['raw_participants', index, 'dni'], errors) && this.getNestedObject(['raw_participants', index, 'dni'], touched)}
+                                  helperText={this.getNestedObject(['raw_participants', index, 'dni'], errors) && this.getNestedObject(['raw_participants', index, 'dni'], touched) ? this.getNestedObject(['raw_participants', index, 'dni'], errors) : null} />)}
+                            />
+                          </TableCell>
+                          <TableCell>
+                            <Field
+                              name={`raw_participants.${index}.grade_choices`}
+                              render={({ field }) => (
+                                <TextField {...field} select label='Año *'
+                                  error={this.getNestedObject(['raw_participants', index, 'grade_choices'], errors) && this.getNestedObject(['raw_participants', index, 'grade_choices'], touched)}
+                                  helperText={this.getNestedObject(['raw_participants', index, 'grade_choices'], errors) && this.getNestedObject(['raw_participants', index, 'grade_choices'], touched) ? this.getNestedObject(['grade_choices', index, 'first_name'], errors) : null}
+                                >
+                                  <MenuItem value='3'>7mo</MenuItem>
+                                  <MenuItem value='2'>6to</MenuItem>
+                                  <MenuItem value='1'>5to</MenuItem>
+                                  <MenuItem value='0'>4to</MenuItem>
+                                </TextField>
+                              )}
+                            />
+                          </TableCell>
+                          <TableCell>
+                            <Field
+                              name={`raw_participants.${index}.divition_choices`}
+                              render={({ field }) => (
+                                <TextField {...field} select label='Curso *'
+                                  error={this.getNestedObject(['raw_participants', index, 'divition_choices'], errors) && this.getNestedObject(['raw_participants', index, 'divition_choices'], touched)}
+                                  helperText={this.getNestedObject(['raw_participants', index, 'divition_choices'], errors) && this.getNestedObject(['raw_participants', index, 'divition_choices'], touched) ? this.getNestedObject(['raw_participants', index, 'divition_choices'], errors) : null}
+                                >
+                                  <MenuItem value='2'>"C"</MenuItem>
+                                  <MenuItem value='1'>"B"</MenuItem>
+                                  <MenuItem value='0'>"A"</MenuItem>
+                                </TextField>
+                              )}
+                            />
+                          </TableCell>
+                        </TableRow>
+                      ))}
+                      <TableRow>
+                        <TableCell>
+                          {console.log(arrayHelpers)}
+                          <Button onClick={() => {
+                            arrayHelpers.push({
+                              first_name: '',
+                              last_name: '',
+                              dni: '',
+                              grade_choices: arrayHelpers.form.values.raw_participants[arrayHelpers.form.values.raw_participants.length - 1].grade_choices,
+                              divition_choices: arrayHelpers.form.values.raw_participants[arrayHelpers.form.values.raw_participants.length - 1].divition_choices
+                            })
+                          }}><AddOutlined /></Button>
+                        </TableCell>
+                      </TableRow>
+                    </TableBody>
+                  </Table>
                 )}
               />
-              <Field
-                name='divition_choices'
-                render={({ field }) => (
-                  <TextField {...field} select label='Curso *' variant='outlined'
-                    error={errors.divition_choices && touched.divition_choices}
-                    helperText={errors.divition_choices && touched.divition_choices ? errors.divition_choices : null}
-                  >
-                    <MenuItem value='2'>"C"</MenuItem>
-                    <MenuItem value='1'>"B"</MenuItem>
-                    <MenuItem value='0'>"A"</MenuItem>
-                  </TextField>
-                )}
-              />
-              <Button type='submit'>Añadir</Button>
+              <Button type='submit'>Submit</Button>
             </Form>
           )}
         </Formik>
-        {this.state.rawParticipants.length > 0
-          ? <Table size='small'>
-            <TableHead>
-              <TableCell>Nombre</TableCell>
-              <TableCell>Apellido</TableCell>
-              <TableCell>D.N.I.:</TableCell>
-              <TableCell>Año</TableCell>
-              <TableCell>Curso</TableCell>
-              <TableCell>¿Contacto?</TableCell>
-              <TableCell>Campos opcionales</TableCell>
-              <TableCell>Acciones</TableCell>
-            </TableHead>
-            {this.state.rawParticipants.slice(0).reverse().map((participant, index) => (
-              <TableRow key={index}>
-                <TableCell>{participant.first_name}</TableCell>
-                <TableCell>{participant.last_name}</TableCell>
-                <TableCell>{participant.dni}</TableCell>
-                <TableCell>{participant.grade_choices}</TableCell>
-                <TableCell>{participant.divition_choices}</TableCell>
-                <TableCell>
-                  <Checkbox value={this.state.rawParticipants[index].isChecked} onChange={(e, isChecked) => { this.handleCheck(index, isChecked) }} />
-                </TableCell>
-                <TableCell>
-                  {this.state.rawParticipants[index].isChecked
-                    ? <div>
-                      <TextField name='phone_number' label='Telefono' onChange={(e) => { this.handleUpdate(e, index) }} />
-                      <TextField name='email' label='Email' onChange={(e) => { this.handleUpdate(e, index) }} />
-                    </div>
-                    : <div>
-                      <TextField disabled label='Telefono' />
-                      <TextField disabled label='Email' />
-                    </div>
-                  }
-                </TableCell>
-                <TableCell>
-                  <Button onClick={() => { this.handleDelete(index) }}>Borrar</Button>
-                </TableCell>
-              </TableRow>))
-            }
-          </Table> : null}
       </Paper>
     )
   }
