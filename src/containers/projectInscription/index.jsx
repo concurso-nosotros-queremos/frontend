1fc795c63467d6bb98eb8a3ca8784c3d4fcf4d4d
Francesco Silvetti <francescosilvetti8@gmail.com>
2019-07-01 23:18:51 -0300

    Add form fragments to InscrptionWrapper

--- src/containers/projectInscription/index.jsx
+++ src/containers/projectInscription/index.jsx
@@ -4,9 +4,13 @@ import * as Yup from 'yup'
 import { Paper } from '@material-ui/core'
 import Button from '@material-ui/core/Button'
 import ParticipantsWrapper, { participantsSchema } from './forms/participantsWrapper'
+import LocationWrapper, { locationSchema } from './forms/group/locationWrapper'
+import SchoolWrapper, { schoolSchema } from './forms/group/schoolWrapper'
 
 const validationSchema = Yup.object().shape({
-  raw_participants: participantsSchema
+  raw_participants: participantsSchema,
+  group_location: locationSchema,
+  raw_school: schoolSchema
 })
 
 const initialValues = {
@@ -16,7 +20,20 @@ const initialValues = {
     dni: '',
     grade_choices: '',
     divition_choices: ''
-  }]
+  }],
+  group_location: {
+    street_name: '',
+    street_number: '',
+    zip_code: '',
+    city: ''
+  },
+  raw_school: {
+    name: '',
+    address: '',
+    principal_name: '',
+    school_types: '',
+    com_preference: ''
+  }
 }
 
 export default class InscriptionWrapper extends Component {
@@ -36,12 +53,15 @@ export default class InscriptionWrapper extends Component {
           initialValues={initialValues}
           validationSchema={validationSchema}
           onSubmit={(values) => {
-            window.alert(JSON.stringify(values, 0, 2))
+            console.log(JSON.stringify(values, 0, 2))
           }}
         >
           {({ errors, touched }) => (
             <Form>
-              {[<ParticipantsWrapper errors={errors} touched={touched} />][this.state.index]}
+              {[<ParticipantsWrapper errors={errors} touched={touched} />,
+                <LocationWrapper errors={errors} touched={touched} />,
+                <SchoolWrapper errors={errors} touched={touched} />
+              ]}
               <Button type='submit'>Submit</Button>
             </Form>
           )}
