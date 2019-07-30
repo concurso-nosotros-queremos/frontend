92dcd39c2058916b7eee171a47e1dd9732c53b9c
Martin Olivari <martin.olivari01@gmail.com>
2019-07-30 20:19:01 -0300

    New navbar added

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,230 +1,127 @@
-import React from 'react'
-import { fade, makeStyles } from '@material-ui/core/styles'
-import AppBar from '@material-ui/core/AppBar'
-import Toolbar from '@material-ui/core/Toolbar'
-import IconButton from '@material-ui/core/IconButton'
-import Typography from '@material-ui/core/Typography'
-import InputBase from '@material-ui/core/InputBase'
-import Badge from '@material-ui/core/Badge'
-import MenuItem from '@material-ui/core/MenuItem'
-import Menu from '@material-ui/core/Menu'
-import MenuIcon from '@material-ui/icons/Menu'
-import SearchIcon from '@material-ui/icons/Search'
-import AccountCircle from '@material-ui/icons/AccountCircle'
-import MailIcon from '@material-ui/icons/Mail'
-import NotificationsIcon from '@material-ui/icons/Notifications'
-import MoreIcon from '@material-ui/icons/MoreVert'
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Button from '@material-ui/core/Button';
+import Hidden from '@material-ui/core/Hidden';
+import Grid from '@material-ui/core/Grid';
+import IconButton from '@material-ui/core/IconButton';
+import Modal from '@material-ui/core/Modal';
+import Paper from '@material-ui/core/Paper';
+import Fade from '@material-ui/core/Fade';
+import MenuIcon from '@material-ui/icons/Menu';
+import CloseIcon from '@material-ui/icons/Close';
 
 const useStyles = makeStyles(theme => ({
-  grow: {
-    flexGrow: 1
+  appBar: {
+    flexGrow: 1,
+    backgroundColor: "rgba(0,0,0,0)",
+    boxShadow: "none",
   },
-  menuButton: {
-    marginRight: theme.spacing(2)
+  menuShape: {
+    position: "relative",
+    clipPath: "polygon(100% 0%, 0% 0%, 0% 100%, 50% 70%, 100% 100%);",
+    height: "50px",
+    width: "60px",
+    backgroundColor: "#0B7163",
+    textAlign: "center",
   },
-  title: {
-    display: 'none',
-    [theme.breakpoints.up('sm')]: {
-      display: 'block'
-    }
+  loginButton: {
+    borderRadius: "25px",
+    color: "#4A6572",
+    border: "2px solid #f9aa33",
+    textTransform: "none",
   },
-  search: {
-    position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: fade(theme.palette.common.white, 0.15),
+  appBarButton: {
     '&:hover': {
-      backgroundColor: fade(theme.palette.common.white, 0.25)
+      backgroundColor: "rgba(0,255,0,0)",
     },
-    marginRight: theme.spacing(2),
-    marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      marginLeft: theme.spacing(3),
-      width: 'auto'
-    }
+    margin: 0,
+    padding: 0,
+    height: "70px",
+    position: "relative",
+    paddingRight: "2rem",
+    justifyContent: "start",
+    textTransform: "none",
   },
-  searchIcon: {
-    width: theme.spacing(7),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center'
+  modalButton: {
+    '&:hover': {
+      backgroundColor: "rgba(0,255,0,0)",
+    },
+    textTransform: "none",
   },
-  inputRoot: {
-    color: 'inherit'
+  modal: {
+    borderRadius: "0",
+    display: "flex",
+    height: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+    zIndex: 1
   },
-  inputInput: {
-    padding: theme.spacing(1, 1, 1, 7),
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('md')]: {
-      width: 200
-    }
+  closeIconStyle: {
+    position: "absolute",
+    padding: "4px"
   },
-  sectionDesktop: {
-    display: 'none',
-    [theme.breakpoints.up('md')]: {
-      display: 'flex'
-    }
-  },
-  sectionMobile: {
-    display: 'flex',
-    [theme.breakpoints.up('md')]: {
-      display: 'none'
-    }
-  }
-}))
+}));
 
-export default function PrimarySearchAppBar () {
-  const classes = useStyles()
-  const [anchorEl, setAnchorEl] = React.useState(null)
-  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null)
+export default function NavBar() {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
 
-  const isMenuOpen = Boolean(anchorEl)
-  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl)
+  const handleOpen = () => {
+    setOpen(true);
+  };
 
-  function handleProfileMenuOpen (event) {
-    setAnchorEl(event.currentTarget)
-  }
-
-  function handleMobileMenuClose () {
-    setMobileMoreAnchorEl(null)
-  }
-
-  function handleMenuClose () {
-    setAnchorEl(null)
-    handleMobileMenuClose()
-  }
-
-  function handleMobileMenuOpen (event) {
-    setMobileMoreAnchorEl(event.currentTarget)
-  }
-
-  const menuId = 'primary-search-account-menu'
-  const renderMenu = (
-    <Menu
-      anchorEl={anchorEl}
-      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-      id={menuId}
-      keepMounted
-      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-      open={isMenuOpen}
-      onClose={handleMenuClose}
-    >
-      <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
-      <MenuItem onClick={handleMenuClose}>My account</MenuItem>
-    </Menu>
-  )
-
-  const mobileMenuId = 'primary-search-account-menu-mobile'
-  const renderMobileMenu = (
-    <Menu
-      anchorEl={mobileMoreAnchorEl}
-      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-      id={mobileMenuId}
-      keepMounted
-      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-      open={isMobileMenuOpen}
-      onClose={handleMobileMenuClose}
-    >
-      <MenuItem>
-        <IconButton aria-label='Show 4 new mails' color='inherit'>
-          <Badge badgeContent={4} color='secondary'>
-            <MailIcon />
-          </Badge>
-        </IconButton>
-        <p>Messages</p>
-      </MenuItem>
-      <MenuItem>
-        <IconButton aria-label='Show 11 new notifications' color='inherit'>
-          <Badge badgeContent={11} color='secondary'>
-            <NotificationsIcon />
-          </Badge>
-        </IconButton>
-        <p>Notifications</p>
-      </MenuItem>
-      <MenuItem onClick={handleProfileMenuOpen}>
-        <IconButton
-          aria-label='Account of current user'
-          aria-controls='primary-search-account-menu'
-          aria-haspopup='true'
-          color='inherit'
-        >
-          <AccountCircle />
-        </IconButton>
-        <p>Profile</p>
-      </MenuItem>
-    </Menu>
-  )
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   return (
-    <div className={classes.grow}>
-      <AppBar position='static'>
-        <Toolbar>
-          <IconButton
-            edge='start'
-            className={classes.menuButton}
-            color='inherit'
-            aria-label='Open drawer'
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography className={classes.title} variant='h6' noWrap>
-            Material-UI
-          </Typography>
-          <div className={classes.search}>
-            <div className={classes.searchIcon}>
-              <SearchIcon />
-            </div>
-            <InputBase
-              placeholder='Search…'
-              classes={{
-                root: classes.inputRoot,
-                input: classes.inputInput
-              }}
-              inputProps={{ 'aria-label': 'Search' }}
-            />
-          </div>
-          <div className={classes.grow} />
-          <div className={classes.sectionDesktop}>
-            <IconButton aria-label='Show 4 new mails' color='inherit'>
-              <Badge badgeContent={4} color='secondary'>
-                <MailIcon />
-              </Badge>
+    <React.Fragment>
+      <AppBar className={classes.appBar} position="fixed">
+        <Toolbar style={{ justifyContent: "space-between" }}>
+          <Hidden smDown>
+            <Grid item style={{ justifyContent: "space-between" }}>
+              <Button disableRipple className={classes.appBarButton}>Acerca</Button>
+              <Button disableRipple className={classes.appBarButton}>Noticias</Button>
+              <Button disableRipple className={classes.appBarButton}>Proyectos</Button>
+              <Button disableRipple className={classes.appBarButton}>Contactos</Button>
+            </Grid>
+            <Grid item style={{ textAlign: "right" }}>
+              <Button className={classes.loginButton} disableRipple size="small"
+                variant="outlined">Iniciar Sesión</Button>
+            </Grid>
+          </Hidden>
+          <Hidden mdUp>
+            <IconButton disableRipple edge="start" color="default" aria-label="menu" onClick={handleOpen}>
+              <MenuIcon />
             </IconButton>
-            <IconButton aria-label='Show 17 new notifications' color='inherit'>
-              <Badge badgeContent={17} color='secondary'>
-                <NotificationsIcon />
-              </Badge>
-            </IconButton>
-            <IconButton
-              edge='end'
-              aria-label='Account of current user'
-              aria-controls={menuId}
-              aria-haspopup='true'
-              onClick={handleProfileMenuOpen}
-              color='inherit'
-            >
-              <AccountCircle />
-            </IconButton>
-          </div>
-          <div className={classes.sectionMobile}>
-            <IconButton
-              aria-label='Show more'
-              aria-controls={mobileMenuId}
-              aria-haspopup='true'
-              onClick={handleMobileMenuOpen}
-              color='inherit'
-            >
-              <MoreIcon />
-            </IconButton>
-          </div>
+            <Modal
+              open={open}
+              onClose={handleClose}>
+              <Fade in={open}>
+                <Paper className={classes.modal}>
+                  <Grid container style={{height: "100%"}}>
+                    <Grid className={classes.closeIconStyle} justify="center" alignItems="center">
+                      <IconButton disableRipple color="inherit" aria-label="close" onClick={handleClose}>
+                        <CloseIcon />
+                      </IconButton>
+                    </Grid>
+                    <Grid container direction="column" justify="center" alignItems="center">
+                      <Button disableRipple className={classes.modalButton}>Acerca</Button>
+                      <Button disableRipple className={classes.modalButton}>Noticias</Button>
+                      <Button disableRipple className={classes.modalButton}>Proyectos</Button>
+                      <Button disableRipple className={classes.modalButton}>Contactos</Button>
+                      <Button className={classes.loginButton} disableRipple size="small"
+                        variant="outlined">Iniciar Sesión</Button>
+                    </Grid>
+                  </Grid>
+                </Paper>
+              </Fade>
+            </Modal>
+          </Hidden>
         </Toolbar>
       </AppBar>
-      {renderMobileMenu}
-      {renderMenu}
-    </div>
-  )
+    </React.Fragment>
+  );
 }
