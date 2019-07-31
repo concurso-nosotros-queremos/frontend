86093a544c952996d8ecf39456a90b30af0f90b4
Juan Cruz <Mare>
2019-07-31 20:47:15 -0300

    default navbar deleted

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { Provider } from 'react-redux'
 import { buildStore } from './state/store'
 import Index from './pages/index'
-import Navbar from './components/navbar'
 import Home from './components/home/Home.jsx'
 
 const store = buildStore()
