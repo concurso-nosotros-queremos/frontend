9f4eb443b89978470e1a3dceb44d24d1cd8434e2
Francesco Silvetti <francescosilvetti8@gmail.com>
2019-08-07 08:53:55 -0300

    Add Grid to LocationWrapper

--- src/containers/projectInscription/forms/locationWrapper.jsx
+++ src/containers/projectInscription/forms/locationWrapper.jsx
@@ -2,41 +2,52 @@ import React, { Component } from 'react'
 import { FastField } from 'formik'
 import TextField from '@material-ui/core/TextField'
 import { errorMessageBuilder, hasError } from './utils'
+import { Grid } from '@material-ui/core'
 
 export default class LocationWrapper extends Component {
   render () {
     return (
-      <div>
+      <>
         <h2>Ubicacion del grupo</h2>
-        <FastField
-          name='group_location.street_name'
-          render={({ field }) => (
-            <TextField {...field} label='Calle *'
-              error={hasError(this.props.errors, this.props.status, this.props.touched, field.name)}
-              helperText={errorMessageBuilder(this.props.errors, this.props.status, this.props.touched, field.name)} />)}
-        />
-        <FastField
-          name='group_location.street_number'
-          render={({ field }) => (
-            <TextField {...field} label='Numero de calle *'
-              error={hasError(this.props.errors, this.props.status, this.props.touched, field.name)}
-              helperText={errorMessageBuilder(this.props.errors, this.props.status, this.props.touched, field.name)} />)}
-        />
-        <FastField
-          name='group_location.zip_code'
-          render={({ field }) => (
-            <TextField {...field} label='Codigo postal'
-              error={hasError(this.props.errors, this.props.status, this.props.touched, field.name)}
-              helperText={errorMessageBuilder(this.props.errors, this.props.status, this.props.touched, field.name)} />)}
-        />
-        <FastField
-          name='group_location.city'
-          render={({ field }) => (
-            <TextField {...field} label='Ciudad *'
-              error={hasError(this.props.errors, this.props.status, this.props.touched, field.name)}
-              helperText={errorMessageBuilder(this.props.errors, this.props.status, this.props.touched, field.name)} />)}
-        />
-      </div>
+        <Grid container spacing={2}>
+          <Grid item xs={6}>
+            <FastField
+              name='group_location.street_name'
+              render={({ field }) => (
+                <TextField {...field} fullWidth variant='outlined' label='Calle'
+                  error={hasError(this.props.errors, this.props.status, this.props.touched, field.name)}
+                  helperText={errorMessageBuilder(this.props.errors, this.props.status, this.props.touched, field.name)} />)}
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <FastField
+              name='group_location.street_number'
+              render={({ field }) => (
+                <TextField {...field} fullWidth variant='outlined' label='Numero de calle'
+                  error={hasError(this.props.errors, this.props.status, this.props.touched, field.name)}
+                  helperText={errorMessageBuilder(this.props.errors, this.props.status, this.props.touched, field.name)} />)}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <FastField
+              name='group_location.zip_code'
+              render={({ field }) => (
+                <TextField {...field} fullWidth variant='outlined' label='Codigo postal'
+                  error={hasError(this.props.errors, this.props.status, this.props.touched, field.name)}
+                  helperText={errorMessageBuilder(this.props.errors, this.props.status, this.props.touched, field.name)} />)}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <FastField
+              name='group_location.city'
+              render={({ field }) => (
+                <TextField {...field} fullWidth variant='outlined' label='Ciudad'
+                  error={hasError(this.props.errors, this.props.status, this.props.touched, field.name)}
+                  helperText={errorMessageBuilder(this.props.errors, this.props.status, this.props.touched, field.name)} />)}
+            />
+          </Grid>
+        </Grid>
+      </>
     )
   }
 }
