b62c1c0fbb4398686b815136d1935f50571448ab
TinchoOlivari <martin.olivari01@gmail.com>
2019-08-14 12:48:41 -0300

    Container padre agregado

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 import NavBar from '../../components/navbar'
 import Home from '../../components/home'
 import InscriptionWrapper from '../../containers/projectInscription'
-import { Grid } from '@material-ui/core'
+import { Grid, Container } from '@material-ui/core'
 
-export default function Index () {
+export default function Index() {
   return (
-    <>
+    <Container maxWidth="xl">
       <NavBar />
       <Home />
       <Grid container justify='center'>
         <InscriptionWrapper />
       </Grid>
-    </>
+    </Container>
   )
 }
