7fe43b2eaf5069532fbe714ca9c18745dc7fd3e5
TinchoOlivari <martin.olivari01@gmail.com>
2019-08-14 23:19:11 -0300

    ¿Como funciona? v2

--- src/components/comoFunciona/index.jsx
+++ src/components/comoFunciona/index.jsx
@@ -15,6 +15,7 @@ import { ReactComponent as LogoMain } from '../../static/header.svg'
 import { ReactComponent as Logo1 } from '../../static/1.svg'
 import { ReactComponent as Logo2 } from '../../static/2.svg'
 import { ReactComponent as Logo3 } from '../../static/3.svg'
+import { Button } from '@material-ui/core';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -26,12 +27,17 @@ const useStyles = makeStyles(theme => ({
     [theme.breakpoints.up('sm')]: {
       marginTop: '1rem',
       marginBottom: '1rem',
-      paddingLeft: '2rem',
-      paddingRight: '2rem'
+      paddingLeft: '5rem',
+      paddingRight: '5rem'
     },
     [theme.breakpoints.up('lg')]: {
-      paddingLeft: '9rem',
-      paddingRight: '9rem'
+      paddingLeft: '12rem',
+      paddingRight: '12rem'
+    },
+  },
+  pasosContainer:{
+    [theme.breakpoints.down('md')]: {
+      height: 'max-content'
     },
   },
   pasosItem: {
@@ -81,7 +87,7 @@ export default function ComoFunciona() {
         <Grid container wrap="nowrap" className={classes.titleContainer} direction="column" justify="center" alignItems="center">
           <Grid item container style={{ marginBottom: '2rem' }} direction="column" justify="center" alignItems="center">
             <Grid item style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-              <Typography variant='h3' align="center">
+              <Typography variant='h3' align="center" style={{fontWeight:'bold'}}>
                 ¿Como funciona?
             </Typography>
               <div className={classes.barraDecorativa}>
@@ -90,15 +96,68 @@ export default function ComoFunciona() {
           </Grid>
           <Grid item>
             <Typography variant='subtitle1' align="center">
-              El concurso Nosotros Queremos es un concurso anual de emprendimientos que apunta a despertar el espíritu emprendedor de los jovenes en los ultimos años de la secundaria
+              Nosotros Queremos es un concurso anual de emprendimientos que apunta a despertar el espíritu emprendedor de los jovenes en los ultimos años de la secundaria.
             </Typography>
           </Grid>
         </Grid>
         <Grid container className={classes.pasosContainer} direction="row" justify="space-around" alignItems="center">
+          <Grid container item xs={12} sm={3} direction="column" justify="space-evenly" alignItems="center" className={classes.pasosItem} spacing={2}>
+            <Grid item>
+              <Avatar className={classes.avatar}>
+                <Typography variant="h5">1</Typography>
+              </Avatar>
+            </Grid>
+            <Grid item container justify="center" alignItems="center" className={classes.containerImgPasos}>
+              <Logo1 className={classes.imgPasosItem} />
+            </Grid>
+            <Grid item>
+              <Typography variant="h5" style={{ fontWeight: 'bold' }} align="center">Inscribite</Typography>
+            </Grid>
+            <Grid item>
+              <Typography variant="body1" align="center">Tu profe inscribe al grupo de alumn@s</Typography>
+            </Grid>
+          </Grid>
+          <Grid container item xs={12} sm={3} direction="column" justify="space-evenly" alignItems="center" className={classes.pasosItem} spacing={2}>
+            <Grid item>
+              <Avatar className={classes.avatar}>
+                <Typography variant="h5">2</Typography>
+              </Avatar>
+            </Grid>
+            <Grid item container justify="center" alignItems="center" className={classes.containerImgPasos}>
+              <Logo2 className={classes.imgPasosItem} />
+            </Grid>
+            <Grid item>
+              <Typography variant="h5" style={{ fontWeight: 'bold' }} align="center">Te validamos</Typography>
+            </Grid>
+            <Grid item>
+              <Typography variant="body1" align="center">La fundación valida y contesta al grupo</Typography>
+            </Grid>
+          </Grid>
+          <Grid container item xs={12} sm={3} direction="column" justify="space-evenly" alignItems="center" className={classes.pasosItem} spacing={2}>
+            <Grid item>
+              <Avatar className={classes.avatar}>
+                <Typography variant="h5">3</Typography>
+              </Avatar>
+            </Grid>
+            <Grid item container justify="center" alignItems="center" className={classes.containerImgPasos}>
+              <Logo3 className={classes.imgPasosItem} />
+            </Grid>
+            <Grid item>
+              <Typography variant="h5" style={{ fontWeight: 'bold' }}>Participa</Typography>
+            </Grid>
+            <Grid item>
+              <Typography variant="body1" align="center">El grupo participa en el concurso por un premio!</Typography>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid container direction="row" justify="space-around" alignItems="center">
+          <Grid item style={{margin: '1rem'}}>
+            <Button size='large' variant="contained" color="primary" href="#contained-buttons" className={classes.button}>
+              Inscribi a tu grupo
+            </Button>
+          </Grid>
         </Grid>
       </Grid>
-    </Paper>
+    </Paper >
   );
 }
-
-
