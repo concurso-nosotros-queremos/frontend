1bde63ef2c7f77459b2a9128dbde3ea3d265145a
TinchoOlivari <martin.olivari01@gmail.com>
2019-08-17 18:09:21 -0300

    Navbar marginTop changed

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -19,7 +19,7 @@ const useStyles = makeStyles(theme => ({
     backgroundColor: 'transparent !important',
     boxShadow: 'none !important',
     marginBottom: '2rem',
-    paddingTop: '1rem',
+    paddingTop: '3rem',
     [theme.breakpoints.down('sm')]: {
       paddingTop: '0rem !important',
       paddingLeft: '0rem !important',
