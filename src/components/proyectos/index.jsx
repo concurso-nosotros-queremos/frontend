bfe7a28961895d7e2ca0a4c198ad13d69324d8a6
TinchoOlivari <martin.olivari01@gmail.com>
2019-08-17 18:05:07 -0300

    Proyectos added

---
+++ src/components/proyectos/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles/index'
+import Paper from '@material-ui/core/Paper/index'
+import Typography from '@material-ui/core/Typography/index'
+import Grid from '@material-ui/core/Grid'
+import Avatar from '@material-ui/core/Avatar'
+import { Button } from '@material-ui/core'
+
+const useStyles = makeStyles(theme => ({
+
+}))
+
+export default function ComoFunciona() {
+    const classes = useStyles()
+
+    return (
+        <>
+        </>
+    );
+}
--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -2,12 +2,18 @@ import React from 'react'
 import NavBar from '../../components/navbar/index'
 import Home from '../../components/home/index'
 import ComoFunciona from '../../components/comoFunciona/index'
+import Proyectos from '../../components/proyectos/index'
 import InscriptionWrapper from '../../containers/projectInscription/index'
 import { Grid, Container } from '@material-ui/core'
 import { makeStyles } from '@material-ui/styles';
 
 const useStyles = makeStyles(theme => ({
   container: {
+    paddingLeft: '6rem',
+    paddingRight: '6rem',
+    [theme.breakpoints.down('sm')]: {
+      height: 'auto',
+    }
   }
 }))
 
@@ -18,6 +24,7 @@ export default function Index() {
     <Container maxWidth="xl" className={classes.container}>
       <NavBar />
       <ComoFunciona />
+      <Proyectos />
     </Container>
   )
 }
