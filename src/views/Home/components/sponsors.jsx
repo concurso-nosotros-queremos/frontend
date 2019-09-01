54f00825e6f9c6befdd5c38f5b7d0d92ecf06403
TinchoOlivari <martin.olivari01@gmail.com>
2019-09-01 00:18:55 -0300

    Sponsors responsive changed

--- src/views/Home/components/sponsors.jsx
+++ src/views/Home/components/sponsors.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Grid from '@material-ui/core/Grid'
 import { makeStyles } from '@material-ui/core/styles'
+import { Typography } from '@material-ui/core'
 
 const useStyles = makeStyles(theme => ({
-  root: {
-    [theme.breakpoints.down('sm')]: {
-      justifyContent: 'center'
-    }
+  root:{
+    [theme.breakpoints.only('xs')]: {
+      flexDirection: 'column'
+    },
   },
   imgSponsor: {
     maxWidth: '100%',
@@ -22,15 +23,31 @@ const useStyles = makeStyles(theme => ({
     [theme.breakpoints.up('sm')]: {
       height: '4rem'
     }
-  }
+  },
+  barraDecorativa: {
+    width: '50%',
+    height: '5px',
+    backgroundColor: theme.palette.secondary.main,
+    borderRadius: '5px',
+    marginTop: '.6rem'
+  },
 }))
 
-export default function Sponsors () {
+export default function Sponsors() {
   const classes = useStyles()
 
   return (
     <>
-      <Grid container direction='row' justify='space-between' className={classes.root} id='sponsors'>
+      <Grid container wrap='nowrap' direction='column' justify='center' alignItems='center' style={{ marginBottom: '3rem' }}>
+        <Grid item style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+          <Typography variant='h3' align='center' style={{ fontWeight: 'bold' }}>
+            Sponsors
+            </Typography>
+          <div className={classes.barraDecorativa} />
+        </Grid>
+      </Grid>
+
+      <Grid container direction='row' justify='space-around' className={classes.root} id='sponsors'>
         <Grid item xs={6} sm='auto' className={classes.imgItem}>
           <img className={classes.imgSponsor} alt='sponsor' src={require('../../../assets/sponsors/Luigi-Bosca.jpg')} />
         </Grid>
