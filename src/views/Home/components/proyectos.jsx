25609f1013f47020b3e2ee068b1f3a6d66433c1d
TinchoOlivari <martin.olivari01@gmail.com>
2019-09-03 12:34:41 -0300

    Proyectos maping data and ellipsis package added

--- src/views/Home/components/proyectos.jsx
+++ src/views/Home/components/proyectos.jsx
@@ -14,6 +14,10 @@ const useStyles = makeStyles(theme => ({
     marginTop: '.6rem'
   },
   card: {
+    display: 'flex',
+    flexDirection: 'column',
+    maxHeight: '26rem',
+    height: "100%",
     borderRadius: '10px'
   },
   contenedorCards: {
@@ -43,6 +47,27 @@ const useStyles = makeStyles(theme => ({
   }
 }))
 
+const data = [
+  {
+    name: 'Nombre bonito',
+    solution: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letrap',
+    url: '/projects/3',
+    image_url: 'https://source.unsplash.com/random/500x200',
+  },
+  {
+    name: 'Nombre bonito',
+    solution: 'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica.',
+    url: '/projects/4',
+    image_url: 'https://source.unsplash.com/random/500x200',
+  },
+  {
+    name: 'Nombre bonito',
+    solution: 'Lizards',
+    url: '/projects/5',
+    image_url: 'https://source.unsplash.com/random/500x200',
+  },
+]
+
 export default function Proyectos() {
   const classes = useStyles()
 
@@ -59,101 +84,34 @@ export default function Proyectos() {
         </Grid>
 
         <Grid container direction='row' justify='space-between' alignItems='center' className={classes.contenedorCards}>
-          <Grid item className={classes.itemCard}>
+          {data.map((el, idx) =>
             <Box boxShadow={5} className={classes.itemCard}>
               <Card className={classes.card}>
                 <CardMedia
                   component='img'
                   alt='Contemplative Reptile'
                   height='140'
-                  image={require('../../../assets/Estudiantes.jpg')}
+                  image={el.image_url}
                 />
                 <CardContent>
-                  <Typography gutterBottom variant='h5'>Animarse a más</Typography>
+                  <Typography gutterBottom variant='h5'>{el.name}</Typography>
                   <Typography variant='body1' component='p'>
-                    Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                    across all continents except Antarctica. Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                    across all continents except Antarctica
                   </Typography>
                 </CardContent>
 
                 <CardActions>
                   <Grid container justify='flex-end' alignItems='center'>
                     <Grid item>
-                      <Button size='small' color='secondary'>
+                      <Button size='small' color='secondary' href={el.url}>
                         Conocer mas
-                      </Button>
+                    </Button>
                     </Grid>
                   </Grid>
                 </CardActions>
               </Card>
             </Box>
-          </Grid>
-          <Grid item className={classes.itemCard}>
-            <Box boxShadow={5} className={classes.itemCard}>
-              <Card className={classes.card}>
-                <CardMedia
-                  component='img'
-                  alt='Contemplative Reptile'
-                  height='140'
-                  image={require('../../../assets/Estudiantes.jpg')}
-                />
-                <CardContent>
-                  <Typography gutterBottom variant='h5'>Animarse a más</Typography>
-                  <Typography variant='body1' component='p'>
-                    Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                    across all continents except Antarctica. Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                    across all continents except Antarctica
-                  </Typography>
-                </CardContent>
-                <CardActions>
-                  <Grid container justify='flex-end' alignItems='center'>
-                    <Grid item>
-                      <Button size='small' color='secondary'>
-                        Conocer mas
-                      </Button>
-                    </Grid>
-                  </Grid>
-                </CardActions>
-              </Card>
-            </Box>
-          </Grid>
-          <Hidden smDown>
-            <Grid item className={classes.itemCard}>
-              <Box boxShadow={5} className={classes.itemCard}>
-                <Card className={classes.card}>
-                  <CardMedia
-                    component='img'
-                    alt='Contemplative Reptile'
-                    height='140'
-                    image={require('../../../assets/Estudiantes.jpg')}
-                  />
-                  <CardContent>
-                    <Typography gutterBottom variant='h5'>
-                      Animarse a más
-                      </Typography>
-                    <Typography variant='body1' component='p'>
-                      Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                      across all continents except Antarctica. Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                      across all continents except Antarctica
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Grid container justify='flex-end' alignItems='center'>
-                      <Grid item>
-                        <Button size='small' color='secondary'>
-                          Conocer mas
-                        </Button>
-                      </Grid>
-                    </Grid>
-                  </CardActions>
-                </Card>
-              </Box>
-            </Grid>
-
-          </Hidden>
+          )}
         </Grid>
-
         <Grid container direction='row' justify='center' alignItems='center'>
           <Grid item style={{ margin: '1rem 1rem 0rem 1rem' }}>
             <Button size='large' variant='contained' color='primary' href='/ver-proyectos'>
