a7ba37163f5e9e9ea9f52bc0fc7581ae0bf59e7a
Francesco Silvetti <francescosilvetti8@gmail.com>
2019-09-04 17:50:57 -0300

    SchoolWrapper to functional component

--- src/containers/projectInscription/forms/schoolWrapper.jsx
+++ src/containers/projectInscription/forms/schoolWrapper.jsx
@@ -1,90 +1,90 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { FastField } from 'formik'
 import TextField from '@material-ui/core/TextField'
 import MenuItem from '@material-ui/core/MenuItem'
 import { errorMessageBuilder, hasError } from './_utils'
-import { Grid, Fade } from '@material-ui/core'
+import { Grid, Fade, Typography } from '@material-ui/core'
 
-export default class SchoolWrapper extends Component {
-  render () {
-    return (
-      <Fade in mountOnEnter unmountOnExit>
-        <div>
-          <h2>Información de la escuela</h2>
-          <Grid container spacing={2}>
-            <Grid item xs={12}>
-              <FastField
-                name='raw_school.name'
-                render={({ field }) => (
-                  <TextField {...field}
-                    fullWidth
-                    variant='outlined'
-                    label='Nombre'
-                    error={hasError(this.props.errors, this.props.status, this.props.touched, field.name)}
-                    helperText={errorMessageBuilder(this.props.errors, this.props.status, this.props.touched, field.name)} />)}
-              />
-            </Grid>
-            <Grid item xs={12} sm={8}>
-              <FastField
-                name='raw_school.street_name'
-                render={({ field }) => (
-                  <TextField {...field}
-                    fullWidth
-                    variant='outlined'
-                    label='Nombre de la calle'
-                    error={hasError(this.props.errors, this.props.status, this.props.touched, field.name)}
-                    helperText={errorMessageBuilder(this.props.errors, this.props.status, this.props.touched, field.name)} />)}
-              />
-            </Grid>
-            <Grid item xs={12} sm={4}>
-              <FastField
-                name='raw_school.street_number'
-                render={({ field }) => (
-                  <TextField {...field}
-                    fullWidth
-                    variant='outlined'
-                    label='Altura'
-                    error={hasError(this.props.errors, this.props.status, this.props.touched, field.name)}
-                    helperText={errorMessageBuilder(this.props.errors, this.props.status, this.props.touched, field.name)} />)}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <FastField
-                name='raw_school.city'
-                render={({ field }) => (
-                  <TextField {...field}
-                    fullWidth
-                    variant='outlined'
-                    label='Ciudad'
-                    error={hasError(this.props.errors, this.props.status, this.props.touched, field.name)}
-                    helperText={errorMessageBuilder(this.props.errors, this.props.status, this.props.touched, field.name)} />)}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <FastField
-                name='raw_school.school_types'
-                render={({ field }) => (
-                  <TextField {...field}
-                    select
-                    fullWidth
-                    variant='outlined'
-                    label='Tipo de escuela'
-                    error={hasError(this.props.errors, this.props.status, this.props.touched, field.name)}
-                    helperText={errorMessageBuilder(this.props.errors, this.props.status, this.props.touched, field.name)}
-                  >
-                    <MenuItem value='5'>Residencia</MenuItem>
-                    <MenuItem value='4'>Escuela Rural</MenuItem>
-                    <MenuItem value='3'>Tecnica Privada</MenuItem>
-                    <MenuItem value='2'>Tecnica Publica</MenuItem>
-                    <MenuItem value='1'>Privada</MenuItem>
-                    <MenuItem value='0'>Publica</MenuItem>
-                  </TextField>
-                )}
-              />
-            </Grid>
+const SchoolWrapper = props => {
+  return (
+    <Fade in mountOnEnter unmountOnExit>
+      <div>
+        <Typography variant='h6'>Información de la escuela</Typography>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <FastField
+              name='raw_school.name'
+              render={({ field }) => (
+                <TextField {...field}
+                  fullWidth
+                  variant='outlined'
+                  label='Nombre'
+                  error={hasError(props.errors, props.status, props.touched, field.name)}
+                  helperText={errorMessageBuilder(props.errors, props.status, props.touched, field.name)} />)}
+            />
           </Grid>
-        </div>
-      </Fade>
-    )
-  }
+          <Grid item xs={12} sm={8}>
+            <FastField
+              name='raw_school.street_name'
+              render={({ field }) => (
+                <TextField {...field}
+                  fullWidth
+                  variant='outlined'
+                  label='Nombre de la calle'
+                  error={hasError(props.errors, props.status, props.touched, field.name)}
+                  helperText={errorMessageBuilder(props.errors, props.status, props.touched, field.name)} />)}
+            />
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <FastField
+              name='raw_school.street_number'
+              render={({ field }) => (
+                <TextField {...field}
+                  fullWidth
+                  variant='outlined'
+                  label='Altura'
+                  error={hasError(props.errors, props.status, props.touched, field.name)}
+                  helperText={errorMessageBuilder(props.errors, props.status, props.touched, field.name)} />)}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <FastField
+              name='raw_school.city'
+              render={({ field }) => (
+                <TextField {...field}
+                  fullWidth
+                  variant='outlined'
+                  label='Ciudad'
+                  error={hasError(props.errors, props.status, props.touched, field.name)}
+                  helperText={errorMessageBuilder(props.errors, props.status, props.touched, field.name)} />)}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <FastField
+              name='raw_school.school_types'
+              render={({ field }) => (
+                <TextField {...field}
+                  select
+                  fullWidth
+                  variant='outlined'
+                  label='Tipo de escuela'
+                  error={hasError(props.errors, props.status, props.touched, field.name)}
+                  helperText={errorMessageBuilder(props.errors, props.status, props.touched, field.name)}
+                >
+                  <MenuItem value='5'>Residencia</MenuItem>
+                  <MenuItem value='4'>Escuela Rural</MenuItem>
+                  <MenuItem value='3'>Tecnica Privada</MenuItem>
+                  <MenuItem value='2'>Tecnica Publica</MenuItem>
+                  <MenuItem value='1'>Privada</MenuItem>
+                  <MenuItem value='0'>Publica</MenuItem>
+                </TextField>
+              )}
+            />
+          </Grid>
+        </Grid>
+      </div>
+    </Fade>
+  )
 }
+
+export default SchoolWrapper
