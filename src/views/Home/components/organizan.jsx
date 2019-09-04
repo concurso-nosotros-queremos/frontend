3d34656b6b7eb10583f7a6e78751456841f4032b
TinchoOlivari <martin.olivari01@gmail.com>
2019-09-03 23:40:26 -0300

    Organizan now maping fake data

--- src/views/Home/components/organizan.jsx
+++ src/views/Home/components/organizan.jsx
@@ -5,6 +5,10 @@ import Grid from '@material-ui/core/Grid'
 import { Box, Avatar } from '@material-ui/core'
 import Background from '../../../assets/organizanImage.svg'
 
+import persona1 from '../../../assets/persona1.jpg'
+import persona2 from '../../../assets/persona2.png'
+import persona3 from '../../../assets/persona3.jpg'
+
 const useStyles = makeStyles(theme => ({
   root: {
     display: 'flex',
@@ -65,6 +69,24 @@ const useStyles = makeStyles(theme => ({
   },
 }))
 
+const data = [
+  {
+    nombre: 'Virginia Barbera',
+    cargo: 'Directora general',
+    image_url: persona1,
+  },
+  {
+    nombre: 'Maria Garcia',
+    cargo: 'Coordinadora Nosotros Queremos',
+    image_url: persona2,
+  },
+  {
+    nombre: 'Mateo Perez',
+    cargo: 'Coordinadora Nosotros Queremos',
+    image_url: persona3,
+  },
+]
+
 export default function Organizan() {
   const classes = useStyles()
 
@@ -83,37 +105,19 @@ export default function Organizan() {
         <Grid container item xs={12} sm={10} md={6} lg={6} direction='row' justify='flex-start' className={classes.cardOrganizan}>
           <Box boxShadow={6} style={{ width: '100%', padding: '2rem', borderRadius: '10px' }}>
             <Grid container direction='column' justify='space-between' style={{ minHeight: '100%' }}>
-              <Grid container direction='row' justify='flex-start' alignItems='center' wrap='nowrap'>
-                <Grid item>
-                  <Avatar alt='Remy Sharp' src={require('../../../assets/persona1.jpg')} className={classes.avatarImg} />
-                </Grid>
-                <Grid item className={classes.txtOrganizadores}>
-                  <Typography variant='h5'>Virginia Barbera</Typography>
-                  <Typography variant='subtitle1'>Directora general</Typography>
-                </Grid>
-              </Grid>
-              <Grid container direction='row' justify='flex-start' alignItems='center' wrap='nowrap'>
-                <Grid item>
+
+              {data.map((el, idx) =>
+                <Grid container direction='row' justify='flex-start' alignItems='center' wrap='nowrap'>
                   <Grid item>
-                    <Avatar alt='Remy Sharp' src={require('../../../assets/persona2.png')} className={classes.avatarImg} />
+                    <Avatar alt='Remy Sharp' src={el.image_url} className={classes.avatarImg} />
+                  </Grid>
+                  <Grid item className={classes.txtOrganizadores}>
+                    <Typography variant='h5'>{el.nombre}</Typography>
+                    <Typography variant='subtitle1'>{el.cargo}</Typography>
                   </Grid>
                 </Grid>
-                <Grid item className={classes.txtOrganizadores}>
-                  <Typography variant='h5'>Maria Garcia</Typography>
-                  <Typography variant='subtitle1'>Coordinadora Nosotros Queremos</Typography>
-                </Grid>
-              </Grid>
-              <Grid container direction='row' justify='flex-start' alignItems='center' wrap='nowrap'>
-                <Grid item>
-                  <Grid item>
-                    <Avatar alt='Remy Sharp' src={require('../../../assets/persona3.jpg')} className={classes.avatarImg} />
-                  </Grid>
-                </Grid>
-                <Grid item className={classes.txtOrganizadores}>
-                  <Typography variant='h5'>Mateo Perez</Typography>
-                  <Typography variant='subtitle1'>Coordinador Nosotros Queremos</Typography>
-                </Grid>
-              </Grid>
+              )}
+
             </Grid>
           </Box>
         </Grid>
