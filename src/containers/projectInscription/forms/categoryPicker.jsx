7b504f0807b8cb40ba613ccf3742fccd925ee847
juansa6201 <jcmare18@gmail.com>
2019-10-15 08:48:07 -0300

    categoryPicker added

---
+++ src/containers/projectInscription/forms/categoryPicker.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { Formik, FieldArray } from 'formik'
+
+console.clear()
+
+const categories = [
+  { id: 1, name: 'Medio Ambiente' },
+  { id: 2, name: 'Adicciones' },
+  { id: 3, name: 'Apoyo a instituciones de menores' },
+  { id: 4, name: 'Apoyo a instituciones de tercera edad' },
+  { id: 5, name: 'Mejoramiento de espacios de uso comun' },
+  { id: 6, name: 'Seguridad vial' },
+  { id: 7, name: 'Genero' },
+  { id: 8, name: 'Otro' }
+]
+
+const CategoryPicker = () => (
+  <FieldArray
+    name='raw_project.category'
+    render={arrayHelpers => (
+      <div className={{ display: 'flex' }}>
+        {categories.map(category => (
+          <div key={category.id}>
+            <label>
+              <input
+                name='raw_projet.category'
+                type='checkbox'
+                value={category.id}
+                checked={arrayHelpers.form.values.raw_project.category.includes(category.id)}
+                onChange={e => {
+                  if (e.target.checked) arrayHelpers.push(category.id)
+                  else {
+                    const idx = arrayHelpers.form.values.raw_project.category.indexOf(category.id)
+                    arrayHelpers.remove(idx)
+                  }
+                }}
+              />{' '}
+              {category.name}
+            </label>
+          </div>
+        ))}
+      </div>
+    )}
+  />
+)
+
+export default CategoryPicker
