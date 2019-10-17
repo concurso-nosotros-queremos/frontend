846538ba908e76f257ec0c3ef95a9481edcca459
Martin Olivari <martin.olivari01@gmail.com>
2019-10-16 21:02:59 -0300

    TimePicker added and some other stuff

--- src/App.js
+++ src/App.js
@@ -5,18 +5,13 @@ import { ThemeProvider } from '@material-ui/styles'
 import theme from './theme/theme'
 import MainRouter from './Routes'
 
-import { MuiPickersUtilsProvider } from '@material-ui/pickers'
-import MomentUtils from '@date-io/moment'
-
 const store = buildStore()
 
 function App(props) {
   return (
     <Provider store={store}>
       <ThemeProvider theme={theme}>
-        <MuiPickersUtilsProvider utils={MomentUtils}>
-          <MainRouter />
-        </MuiPickersUtilsProvider>
+        <MainRouter />
       </ThemeProvider>
     </Provider>
   )
--- src/views/Dashboard/components/Dialog.jsx
+++ src/views/Dashboard/components/Dialog.jsx
@@ -1,13 +1,32 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/styles'
+import useMediaQuery from '@material-ui/core/useMediaQuery'
+import { useTheme } from '@material-ui/core/styles'
 import Button from '@material-ui/core/Button'
+import { Grid, DialogContent } from '@material-ui/core'
+
 import DialogTitle from '@material-ui/core/DialogTitle'
 import Dialog from '@material-ui/core/Dialog'
-import { DatePicker } from "@material-ui/pickers"
+import DialogActions from '@material-ui/core/DialogActions'
+import { DatePicker, TimePicker, MuiPickersUtilsProvider } from '@material-ui/pickers'
 
-export default function SimpleDialogDemo(props) {
+
+import MomentUtils from '@date-io/moment'
+import Moment from 'moment'
+import "moment/locale/es"
+
+Moment.locale("es")
+
+const useStyles = makeStyles(theme => ({
+
+}))
+
+export default function SimpleDialog(props) {
+  const classes = useStyles()
   const [open, setOpen] = React.useState(false)
-  const [selectedDate, setSelectedDate] = React.useState(new Date());
-  const [date, changeDate] = React.useState(new Date());
+  const [selectedDate, setSelectedDate] = React.useState(new Date())
+  const theme = useTheme()
+  const fullScreen = useMediaQuery(theme.breakpoints.only('xs'));
 
   const handleClickOpen = () => {
     setOpen(true)
@@ -16,29 +35,57 @@ export default function SimpleDialogDemo(props) {
     setOpen(false)
   }
   const handleDateChange = date => {
-    setSelectedDate(date);
-  };
+    setSelectedDate(date)
+    //console.log(Moment(date).format('YYYY-MM-DDTHH:mm:ss[Z]'))
+  }
 
   return (
     <>
-      <Button size='small' variant='contained' className={props.estilos} onClick={handleClickOpen}>
+      <Button size='small' variant='contained' className={props.estilos} onClick={handleClickOpen} maxWidth={'xl'}>
         Extender
       </Button>
-      <Dialog onClose={handleClose} aria-labelledby='simple-dialog-title' open={open}>
-        <DialogTitle id='simple-dialog-title'>Modificar cierre de las incscripciones</DialogTitle>
-
-        <DatePicker
-          autoOk
-          orientation="landscape"
-          variant="static"
-          openTo="date"
-          value={date}
-          onChange={changeDate}
-        />
-
-
+      <Dialog onClose={handleClose} aria-labelledby='simple-dialog-title' open={open} fullScreen={fullScreen} maxWidth="lg">
+        <DialogTitle>
+          <h4 style={{ margin: 0 }}>
+            Modificar cierre de las incscripciones
+          </h4>
+        </DialogTitle>
+        <DialogContent dividers style={{ margin: 0, padding: 0 }}>
+          <Grid container direction='row' justify='flex-start' alignItems='flex-start' className={{}}>
+            <MuiPickersUtilsProvider utils={MomentUtils}>
+              <Grid item xs={12} sm={6}>
+                <DatePicker
+                  format="DD MMMM YYYY"
+                  orientation='portrait'
+                  variant='static'
+                  openTo='date'
+                  value={selectedDate}
+                  onChange={handleDateChange}
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TimePicker
+                  orientation='portrait'
+                  clearable
+                  ampm={false}
+                  label="24 hours"
+                  variant='static'
+                  value={selectedDate}
+                  onChange={handleDateChange}
+                />
+              </Grid>
+            </MuiPickersUtilsProvider>
+          </Grid>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Cancelar
+          </Button>
+          <Button onClick={handleClose} color="primary">
+            Aceptar
+          </Button>
+        </DialogActions>
       </Dialog>
     </>
   )
 }
-
