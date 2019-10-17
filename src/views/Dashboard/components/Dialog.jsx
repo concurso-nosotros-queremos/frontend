e12c262152f8d2f86e96bdf455b52ea24e55197a
Martin Olivari <martin.olivari01@gmail.com>
2019-10-17 13:41:13 -0300

    TImePicker removed

--- src/views/Dashboard/components/Dialog.jsx
+++ src/views/Dashboard/components/Dialog.jsx
@@ -4,13 +4,9 @@ import useMediaQuery from '@material-ui/core/useMediaQuery'
 import { useTheme } from '@material-ui/core/styles'
 import Button from '@material-ui/core/Button'
 import { Grid, DialogContent } from '@material-ui/core'
-
-import DialogTitle from '@material-ui/core/DialogTitle'
 import Dialog from '@material-ui/core/Dialog'
 import DialogActions from '@material-ui/core/DialogActions'
 import { DatePicker, TimePicker, MuiPickersUtilsProvider } from '@material-ui/pickers'
-
-
 import MomentUtils from '@date-io/moment'
 import Moment from 'moment'
 import "moment/locale/es"
@@ -18,7 +14,6 @@ import "moment/locale/es"
 Moment.locale("es")
 
 const useStyles = makeStyles(theme => ({
-
 }))
 
 export default function SimpleDialog(props) {
@@ -26,7 +21,6 @@ export default function SimpleDialog(props) {
   const [open, setOpen] = React.useState(false)
   const [selectedDate, setSelectedDate] = React.useState(new Date())
   const theme = useTheme()
-  const fullScreen = useMediaQuery(theme.breakpoints.only('xs'));
 
   const handleClickOpen = () => {
     setOpen(true)
@@ -44,40 +38,23 @@ export default function SimpleDialog(props) {
       <Button size='small' variant='contained' className={props.estilos} onClick={handleClickOpen} maxWidth={'xl'}>
         Extender
       </Button>
-      <Dialog onClose={handleClose} aria-labelledby='simple-dialog-title' open={open} fullScreen={fullScreen} maxWidth="lg">
-        <DialogTitle>
-          <h4 style={{ margin: 0 }}>
-            Modificar cierre de las incscripciones
-          </h4>
-        </DialogTitle>
-        <DialogContent dividers style={{ margin: 0, padding: 0 }}>
-          <Grid container direction='row' justify='flex-start' alignItems='flex-start' className={{}}>
+      <Dialog onClose={handleClose} aria-labelledby='simple-dialog-title' open={open} maxWidth="lg">
+        <DialogContent style={{ margin: 0, padding: 0 }}>
+          <Grid container direction='row' justify='flex-start' alignItems='flex-start'>
             <MuiPickersUtilsProvider utils={MomentUtils}>
-              <Grid item xs={12} sm={6}>
-                <DatePicker
-                  format="DD MMMM YYYY"
-                  orientation='portrait'
-                  variant='static'
-                  openTo='date'
-                  value={selectedDate}
-                  onChange={handleDateChange}
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TimePicker
-                  orientation='portrait'
-                  clearable
-                  ampm={false}
-                  label="24 hours"
-                  variant='static'
-                  value={selectedDate}
-                  onChange={handleDateChange}
-                />
-              </Grid>
+              <DatePicker
+                inputStyle={{ textAlign: 'center' }}
+                format="DD MMMM YYYY"
+                orientation={useMediaQuery(theme.breakpoints.only("xs")) ? 'portrait' : 'landscape'}
+                variant='static'
+                openTo='date'
+                value={selectedDate}
+                onChange={handleDateChange}
+              />
             </MuiPickersUtilsProvider>
           </Grid>
         </DialogContent>
-        <DialogActions>
+        <DialogActions dividers>
           <Button onClick={handleClose} color="primary">
             Cancelar
           </Button>
