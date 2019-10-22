5b36d15dd636cb12a32a31a8f5970a0dc6fb4629
juansa6201 <jcmare18@gmail.com>
2019-10-22 12:02:24 -0300

    chart data fixed

--- src/hoc/withDashboard.jsx
+++ src/hoc/withDashboard.jsx
@@ -78,7 +78,7 @@ function withDashboard (WrappedComponent, { ...props }) {
         this.setState({ contestEnd: Moment(response[0].inscription_date_to).format('YYYY/MM/DD') })
       })
       getGroup(this.props.token).then(response => {
-        this.setState({ group: response, labels: foo(response.map(el => el.raw_school.state_name))[0], data: foo(response.map(el => el.raw_school.state_name))[1], label_diffusion: foo(response.map(el => el.raw_project.diffusion_name))[0], diffusion: foo(response.map(el => el.raw_project.diffusion))[1], label_school: foo(response.map(el => el.raw_school.school_types_name))[0], school: foo(response.map(el => el.raw_school.school_types))[1], label_participant: countParticipants(response)[0], participant: countParticipants(response)[1] })
+        this.setState({ group: response, labels: foo(response.map(el => el.raw_school.state_name))[0], data: foo(response.map(el => el.raw_school.state_name))[1], label_diffusion: foo(response.map(el => el.raw_project.diffusion_name))[0], diffusion: foo(response.map(el => el.raw_project.diffusion_name))[1], label_school: foo(response.map(el => el.raw_school.school_types_name))[0], school: foo(response.map(el => el.raw_school.school_types_name))[1], label_participant: countParticipants(response)[0], participant: countParticipants(response)[1] })
       })
     }
 
