fb5f09087060758a456020dc86d4d246c47f7149
Martin Olivari <martin.olivari01@gmail.com>
2019-10-27 23:09:14 -0300

    Datatable modified

--- src/views/Groups/Groups.jsx
+++ src/views/Groups/Groups.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { forwardRef } from 'react'
 import { makeStyles } from '@material-ui/styles'
 import { Grid, Table, TableBody, TableCell, TableHead, TableRow } from '@material-ui/core'
 import Card from '@material-ui/core/Card'
 import Typography from '@material-ui/core/Typography'
 import { Link } from 'react-router-dom'
+import MaterialTable from 'material-table'
+
+import {
+  ArrowUpward,
+  ChevronLeft,
+  ChevronRight,
+  Clear,
+  FirstPage,
+  LastPage,
+  Search,
+  EditOutlined
+} from '@material-ui/icons'
+
+const tableIcons = {
+  Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+  ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+  SortArrow: forwardRef((props, ref) => <ArrowUpward {...props} ref={ref} />),
+  FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+  LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+  NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+  PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
+  Edit: forwardRef((props, ref) => <EditOutlined {...props} ref={ref} />),
+}
 
 const useStyles = makeStyles(theme => ({
   card: {
@@ -25,11 +48,10 @@ const useStyles = makeStyles(theme => ({
     padding: '16px'
   },
   title: {
-    fontWeight: 'bold',
     whiteSpace: 'nowrap',
     overflow: 'hidden',
     textOverflow: 'ellipsis',
-    fontSize: '22px'
+    fontSize: '24px'
   },
   tableCellText: {
     fontSize: '14px',
@@ -199,34 +221,33 @@ const Groups = props => {
     <>
       <Grid container direction='row' justify='flex-start' alignItems='flex-start' className={classes.root}>
         <Card className={classes.card} elevation={0}>
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell className={classes.tableCellText}>Nombre</TableCell>
-                <TableCell className={classes.tableCellText} align='center'>Alumnos</TableCell>
-                <TableCell className={classes.tableCellText}>Provincia</TableCell>
-                <TableCell className={classes.tableCellText}>Localidad</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {data.map(row => (
-                <TableRow to={'/groups/' + (row.raw_project.id)} component={Link} className={classes.tableRow}>
-                  <TableCell component='th' scope='row'>
-                    <Typography style={{ fontWeight: 'bold' }}>{row.nombre}</Typography>
-                  </TableCell>
-                  <TableCell align='center' component='th' scope='row'>
-                    {row.total}
-                  </TableCell>
-                  <TableCell component='th' scope='row'>
-                    {row.city}
-                  </TableCell>
-                  <TableCell component='th' scope='row'>
-                    {row.state}
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
+          <MaterialTable
+            icons={tableIcons}
+            columns={[
+              { title: 'Nombre', field: 'name' },
+              { title: 'Alumnos', field: 'alumnos', type: 'numeric' },
+              { title: 'Provincia', field: 'city' },
+              { title: 'Localidad', field: 'state' },
+            ]}
+            data={[
+              { name: 'abEl termoDinamico', alumnos: '1', city: 'jMendoza', state: 'iFray Luis Beltran', },
+              { name: 'bEl termoDinamico', alumnos: '25', city: 'eMendoza', state: 'uFray Luis Beltran', },
+              { name: 'cdEl termoDinamico', alumnos: '3000', city: 'rMendoza', state: 'ytFray Luis Beltran', },
+              { name: 'dEl termoDinamico', alumnos: '67', city: 'opMendoza', state: 'rFray Luis Beltran', },
+              { name: 'zEl termoDinamico', alumnos: '28', city: '23eMendoza', state: 'bFray Luis Beltran', },
+            ]}
+            title={<Typography className={classes.title}>Grupos</Typography>}
+            options={{
+              actionsColumnIndex: -1,
+            }}
+            actions={[
+              {
+                icon: EditOutlined,
+                tooltip: 'Editar grupo',
+                onClick: (event, rowData) => alert("You saved " + rowData.name)
+              }
+            ]}
+          />
         </Card>
       </Grid>
     </>
