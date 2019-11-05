85775bbb2f3dfd4462e65e0b9c9e566623dfbcaa
Francesco Silvetti <francescosilvetti8@gmail.com>
2019-11-05 11:40:35 -0300

    Update working, hardcoded group

--- src/views/EditGroup/EditGroup.jsx
+++ src/views/EditGroup/EditGroup.jsx
@@ -1,28 +1,26 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import { makeStyles } from '@material-ui/styles'
-import { Grid, Card, AppBar, Tabs, Tab, Box, Typography } from '@material-ui/core'
+import { Grid, Card, AppBar, Tabs, Tab, Box, Typography, Button } from '@material-ui/core'
 import { Formik } from 'formik'
 import ParticipantsWrapper from '../../containers/projectInscription/forms/participantsWrapper'
 import ProjectWrapper from '../../containers/projectInscription/forms/projectWrapper'
 import SchoolWrapper from '../../containers/projectInscription/forms/schoolWrapper'
 import ContactWrapper from '../../containers/projectInscription/forms/contactWrapper'
+import fetchResource from '../../services/apiHandler'
 
 const pages = [
   {
-    title: 'Participantes',
-    raw: 'raw_participant'
+    title: 'Participantes'
   },
   {
-    title: 'Escuela',
-    raw: 'raw_school'
+    title: 'Escuela'
   },
   {
-    title: 'Proyecto',
-    raw: 'raw_project'
+    title: 'Proyecto'
   },
   {
-    title: 'Contacto',
-    raw: 'raw_contact'
+    title: 'Contacto'
   }
 ]
 
@@ -140,6 +138,19 @@ const GroupEdit = props => {
     setValue(newValue)
   }
 
+  const handleSubmit = (form, path) => {
+    console.log(form)
+    return fetchResource('rest/group/163/', {
+      method: 'PATCH',
+      headers: {
+        Authorization: `Bearer ${props.token}`
+      },
+      body: {
+        ...form
+      }
+    })
+  }
+
   return (
     <>
       <Grid container direction='row' justify='flex-start' alignItems='flex-start' className={classes.root}>
@@ -152,38 +163,45 @@ const GroupEdit = props => {
               {pages.map((el, idx) => <Tab key={idx} label={el.title} />)}
             </Tabs>
           </AppBar>
-          <TabPanel value={value} index={0}>
-            <Formik
-              initialValues={{ [pages[0].raw]: group[pages[0].raw] }}
-            >
-              <ParticipantsWrapper />
-            </Formik>
-          </TabPanel>
-          <TabPanel value={value} index={1}>
-            <Formik
-              initialValues={{ [pages[1].raw]: group[pages[1].raw] }}
-            >
-              <SchoolWrapper />
-            </Formik>
-          </TabPanel>
-          <TabPanel value={value} index={2}>
-            <Formik
-              initialValues={{ [pages[2].raw]: group[pages[2].raw] }}
-            >
-              <ProjectWrapper />
-            </Formik>
-          </TabPanel>
-          <TabPanel value={value} index={3}>
-            <Formik
-              initialValues={{ [pages[3].raw]: group[pages[3].raw] }}
-            >
-              <ContactWrapper />
-            </Formik>
-          </TabPanel>
+          <Formik
+            onSubmit={(values, { setStatus, setSubmitting }) => {
+              handleSubmit(values).then((response) => {
+                console.log(response)
+              }).catch((error) => {
+                setStatus({ ...error.response })
+                setSubmitting(false)
+              })
+            }}
+            initialValues={group}
+          >
+            {({ errors, touched, status, submitForm }) => (
+              <>
+                <TabPanel value={value} index={0}>
+                  <ParticipantsWrapper />
+                </TabPanel>
+                <TabPanel value={value} index={1}>
+                  <SchoolWrapper />
+                </TabPanel>
+                <TabPanel value={value} index={2}>
+                  <ProjectWrapper />
+                </TabPanel>
+                <TabPanel value={value} index={3}>
+                  <ContactWrapper />
+                </TabPanel>
+                <Button onClick={() => submitForm()}>
+                  Guardar
+                </Button>
+              </>
+            )}
+          </Formik>
         </Card>
       </Grid>
     </>
   )
 }
 
-export default GroupEdit
+const mapStateToProps = (state) => ({
+  token: state.auth.convertedToken.accessToken
+})
+
+export default connect(mapStateToProps)(GroupEdit)
