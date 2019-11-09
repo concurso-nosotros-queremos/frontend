660e69d9ffeb76c9ae13a6ace3ef4e2579cc6b78
Francesco Silvetti <francescosilvetti8@gmail.com>
2019-11-09 18:45:37 -0300

    Most functionality working

--- src/views/EditGroup/EditGroup.jsx
+++ src/views/EditGroup/EditGroup.jsx
@@ -1,6 +1,7 @@
-import React, { useEffect } from 'react'
+import React, { Component } from 'react'
+import { withRouter } from 'react-router'
 import { connect } from 'react-redux'
-import { makeStyles } from '@material-ui/styles'
+import { makeStyles, withStyles } from '@material-ui/styles'
 import { Grid, Card, AppBar, Tabs, Tab, Box, Typography, Button } from '@material-ui/core'
 import { Formik } from 'formik'
 import ParticipantsWrapper from '../../containers/projectInscription/forms/participantsWrapper'
@@ -24,73 +25,6 @@ const pages = [
     title: 'Contacto'
   }
 ]
-
-const group = {
-  raw_school: {
-    id: 163,
-    name: 'ass asd',
-    street_name: 'av china',
-    street_number: 7,
-    city: 527,
-    city_name: 'Agrelo',
-    state_name: 'Mendoza',
-    school_types: 1,
-    school_types_name: 'Privada'
-  },
-  raw_project: {
-    id: 168,
-    name: 'Desarrolo de aplicativo',
-    problem: 'La llegada de los colectivos',
-    solution: 'Crear la app que inidque la ubicacion',
-    diffusion: 0,
-    category: [
-      1,
-      2
-    ],
-    category_name: [
-      {
-        id: 1,
-        name: 'Social',
-        description: 'social'
-      },
-      {
-        id: 2,
-        name: 'Tecnologico',
-        description: 'tech'
-      }
-    ],
-    diffusion_name: 'Mail'
-  },
-  raw_participant: [
-    {
-      id: 276,
-      first_name: 'Juan Cruz',
-      last_name: 'Mare',
-      dni: 43232227,
-      grade_choices: 3
-    },
-    {
-      id: 277,
-      first_name: 'Fran',
-      last_name: 'salvestre',
-      dni: 42573439,
-      grade_choices: 3
-    },
-    {
-      id: 278,
-      first_name: 'Martinsaso',
-      last_name: 'Olivari',
-      dni: 43232222,
-      grade_choices: 3
-    }
-  ],
-  raw_contact: {
-    id: 163,
-    phone_number: 3517724454,
-    alternative_email: 'xd@xd.xd',
-    alternative_phone_number: 3517724454
-  }
-}
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -136,7 +70,46 @@ const TabPanel = props => {
   )
 }
 
-const GroupEdit = props => {
+class EditGroup extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      group: null,
+      error: null
+    }
+
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  componentDidMount () {
+    getOneGroup(this.props.token, this.props.match.params.id)
+      .then(response => this.setState({ group: response }))
+      .catch(error => this.setState({ error: error }))
+  }
+
+  handleSubmit (form) {
+    console.log(form)
+    return fetchResource(`rest/group/${this.props.match.params.id}/`, {
+      method: 'PATCH',
+      headers: {
+        Authorization: `Bearer ${this.props.token}`
+      },
+      body: {
+        ...form
+      }
+    })
+  }
+
+  render () {
+    console.log(this.props)
+    return (
+      <GroupEditor group={this.state.group} onSubmit={this.handleSubmit} />
+    )
+  }
+}
+
+const GroupEditor = props => {
   const classes = useStyles()
 
   const [value, setValue] = React.useState(0)
@@ -145,65 +118,56 @@ const GroupEdit = props => {
     setValue(newValue)
   }
 
-  const handleSubmit = (form, path) => {
-    console.log(form)
-    return fetchResource('rest/group/168/', {
-      method: 'PATCH',
-      headers: {
-        Authorization: `Bearer ${props.token}`
-      },
-      body: {
-        ...form
-      }
-    })
-  }
-
   return (
-    <>
-      <Grid container direction='row' justify='flex-start' alignItems='flex-start' className={classes.root}>
-        <Card className={classes.card} elevation={0}>
-          <Typography className={classes.title} autoCapitalize>
-            Animarse a mas
-          </Typography>
-          <AppBar className={classes.appBar} position='static' color='inherit'>
-            <Tabs value={value} onChange={handleChange} indicatorColor='primary' aria-label='simple tabs example'>
-              {pages.map((el, idx) => <Tab key={idx} label={el.title} />)}
-            </Tabs>
-          </AppBar>
-          <Formik
-            onSubmit={(values, { setStatus, setSubmitting }) => {
-              handleSubmit(values).then((response) => {
-                console.log(response)
-              }).catch((error) => {
-                setStatus({ ...error.response })
-                setSubmitting(false)
-              })
-            }}
-            initialValues={group}
-          >
-            {({ errors, touched, status, submitForm }) => (
-              <>
-                <TabPanel value={value} index={0}>
-                  <ParticipantsWrapper />
-                </TabPanel>
-                <TabPanel value={value} index={1}>
-                  <SchoolWrapper />
-                </TabPanel>
-                <TabPanel value={value} index={2}>
-                  <ProjectWrapper />
-                </TabPanel>
-                <TabPanel value={value} index={3}>
-                  <ContactWrapper />
-                </TabPanel>
-                <Button className={classes.saveButton} onClick={() => submitForm()}>
-                  Guardar
-                </Button>
-              </>
-            )}
-          </Formik>
-        </Card>
-      </Grid>
-    </>
+    <Grid container direction='row' justify='flex-start' alignItems='flex-start' className={classes.root}>
+      <Card className={classes.card} elevation={0}>
+        {props.group
+          ? (
+            <>
+              <Typography className={classes.title} autoCapitalize>
+                {props.group.raw_project.name}
+              </Typography>
+              <AppBar className={classes.appBar} position='static' color='inherit'>
+                <Tabs value={value} onChange={handleChange} indicatorColor='primary' aria-label='simple tabs example'>
+                  {pages.map((el, idx) => <Tab key={idx} label={el.title} />)}
+                </Tabs>
+              </AppBar>
+              <Formik
+                onSubmit={(values, { setStatus, setSubmitting }) => {
+                  props.onSubmit(values).then((response) => {
+                    console.log(response)
+                  }).catch((error) => {
+                    setStatus({ ...error.response })
+                    setSubmitting(false)
+                  })
+                }}
+                initialValues={props.group}
+              >
+                {({ errors, touched, status, submitForm }) => (
+                  <>
+                    <TabPanel value={value} index={0}>
+                      <ParticipantsWrapper />
+                    </TabPanel>
+                    <TabPanel value={value} index={1}>
+                      <SchoolWrapper />
+                    </TabPanel>
+                    <TabPanel value={value} index={2}>
+                      <ProjectWrapper />
+                    </TabPanel>
+                    <TabPanel value={value} index={3}>
+                      <ContactWrapper />
+                    </TabPanel>
+                    <Button className={classes.saveButton} onClick={() => submitForm()}>
+                      Guardar
+                    </Button>
+                  </>
+                )}
+              </Formik>
+            </>
+          )
+          : 'Loading...'}
+      </Card>
+    </Grid>
   )
 }
 
@@ -211,4 +175,4 @@ const mapStateToProps = (state) => ({
   token: state.auth.convertedToken.accessToken
 })
 
-export default connect(mapStateToProps)(GroupEdit)
+export default connect(mapStateToProps)(withRouter(withStyles(useStyles)(EditGroup)))
