35768f730e231102ee4cf8a22866ad99d591a828
Francesco Silvetti <francescosilvetti8@gmail.com>
2019-12-01 10:27:44 -0300

    Change conflictive component name to CheckGroup

--- src/views/Groups/Check.jsx
+++ src/views/Groups/Check.jsx
@@ -16,10 +16,9 @@ const useStyles = makeStyles(theme => ({
   }
 }))
 
-
-const Modal = props => {
+const CheckGroup = props => {
   const classes = useStyles()
-  const [token, setToken] = React.useState("")
+  const [token, setToken] = React.useState('')
   const [err, setErr] = React.useState(null)
   const [rta, setRta] = React.useState(null)
   const [open, setOpen] = React.useState(false)
@@ -31,29 +30,28 @@ const Modal = props => {
     setOpen(false)
     setRta(null)
     setErr(null)
-    setToken("")
+    setToken('')
 
     if (rta === true) {
-      window.location.reload();
+      window.location.reload()
     }
   }
 
   const checkToken = () => {
-    fetch('https://queremosbackend.tk/rest/check/', {
+    window.fetch('https://queremosbackend.tk/rest/check/', {
       method: 'POST',
       headers: {
-        'Accept': 'application/json',
+        Accept: 'application/json',
         'Content-Type': 'application/json',
         Authorization: `Bearer ${props.token}`
       },
       body: JSON.stringify({
-        token: token,
+        token: token
       })
     }).then(response => {
       if (response.status === 400) {
         setErr('Introduzca un token')
-      }
-      else if (response.status === 401) {
+      } else if (response.status === 401) {
         setErr('Ese código no es válido.')
       } else if (response.status === 200) {
         setRta(true)
@@ -67,50 +65,54 @@ const Modal = props => {
 
   return (
     <>
-      <Button variant='outlined' color='primary' style={{ marginTop: '8px', fontSize: "12px" }} onClick={handleClickOpen} type='button'>
+      <Button variant='outlined' color='primary' style={{ marginTop: '8px', fontSize: '12px' }} onClick={handleClickOpen} type='button'>
         Agregar por codigo
       </Button>
       <Dialog onClose={handleClose} open={open} maxWidth='xs'>
-        <DialogTitle style={{ padding: "16px 24px 0px" }}>
-          <Grid container direction="row" justify="flex-start" alignItems="flex-start">
+        <DialogTitle style={{ padding: '16px 24px 0px' }}>
+          <Grid container direction='row' justify='flex-start' alignItems='flex-start'>
             <Grid item>
               <Typography className={classes.title}>
                 Agregar usando codigo
-            </Typography>
+              </Typography>
             </Grid>
             <Grid item>
-              <Typography variant="body1">
+              <Typography variant='body1'>
                 Pedile el codigo de grupo al encargado del grupo
                 al que te queres sumar para poder empezar administrarlo.
-            </Typography>
+              </Typography>
             </Grid>
           </Grid>
         </DialogTitle>
         <DialogContent>
-          {rta ?
-            <Grid container direction='column' justify='center' alignItems='center'>
-              <img src={require('../../assets/green_check.svg')} style={{ width: '5rem' }} alt='check' />
-            </Grid> :
-            <TextField
-              name='token'
-              label='Codigo'
-              fullWidth
-              margin='normal'
-              variant='outlined'
-              onChange={(event) => setToken(event.target.value)}
-              onBlur={() => setErr(null)}
-              value={token}
-              helperText={err}
-              error={err ? true : false}
-            />
-          }
+          {rta
+            ? (
+              <Grid container direction='column' justify='center' alignItems='center'>
+                <img src={require('../../assets/green_check.svg')} style={{ width: '5rem' }} alt='check' />
+              </Grid>
+            )
+            : (
+              <TextField
+                name='token'
+                label='Codigo'
+                fullWidth
+                margin='normal'
+                variant='outlined'
+                onChange={(event) => setToken(event.target.value)}
+                onBlur={() => setErr(null)}
+                value={token}
+                helperText={err}
+                error={!!err}
+              />
+            )}
         </DialogContent>
         <DialogActions>
-          {rta ? null :
-            <Button onClick={checkToken} type='button' style={{ color: "rgba(35, 47, 52, 0.8)" }}>
-              Aceptar
-            </Button>
-          }
+          {rta ? null
+            : (
+              <Button onClick={checkToken} type='button' style={{ color: 'rgba(35, 47, 52, 0.8)' }}>
+                Aceptar
+              </Button>
+            )}
         </DialogActions>
       </Dialog>
     </>
@@ -122,4 +124,4 @@ const mapStateToProps = (state) => ({
   token: state.auth.convertedToken.accessToken
 })
 
-export default connect(mapStateToProps)(withRouter(Modal))
+export default connect(mapStateToProps)(withRouter(CheckGroup))
